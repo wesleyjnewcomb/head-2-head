12e217d1c8bcd8d1be8857df444d9b366b047f5a
adamkgray <kruschka.kruschka@gmail.com>
2019-07-16 20:53:50 +0800

    getting there ...

--- src/app.rb
+++ src/app.rb
@@ -1,7 +1,23 @@
 # Server entry point
 require "sinatra"
 require "mysql2"
+require "inifile"
 
-while true
-    sleep 1
+# try mysql
+begin
+    mysql_cnf = File.join(File.dirname(__FILE__), "db", "my.cfg")
+    mysql_conf = IniFile.new(:filename => mysql_cnf)['client']
+    client = Mysql2::Client.new(
+        :host => mysql_conf["host"],
+        :username => mysql_conf["username"],
+        :password =>mysql_conf["password"]
+    )
+    puts client.info
+rescue StandardError => e
+    puts e.message
+    puts e.backtrace.inspect
+end
+
+get "/" do
+    "hello world"
 end
